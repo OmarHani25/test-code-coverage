762a55908b317c8ecc0c50dadbd52f644d25c105
Eman Mahmoud <152784907+emanmhmod@users.noreply.github.com>
2025-02-10 15:06:14 +0200

    Revert "Merge jest and cypress coverage"

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,52 +1,4 @@
 import { defineConfig } from 'cypress';
-import webpack from '@cypress/webpack-preprocessor';
-import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> {
-  await addCucumberPreprocessorPlugin(on, config);
-  require('@cypress/code-coverage/task')(on, config);
-
-  on(
-    'file:preprocessor',
-    webpack({
-      webpackOptions: {
-        resolve: {
-          extensions: ['.ts', '.js'],
-        },
-        module: {
-          rules: [
-            {
-              test: /\.ts$/,
-              exclude: /node_modules/,
-              use: [
-                {
-                  loader: 'ts-loader',
-                  options: {
-                    transpileOnly: true
-                  }
-                },
-              ],
-            },
-            {
-              test: /\.feature$/,
-              use: [
-                {
-                  loader: '@badeball/cypress-cucumber-preprocessor/webpack',
-                  options: config,
-                },
-              ],
-            },
-          ],
-        },
-      },
-    })
-  );
-  return config;
-}
-
 
 export default defineConfig({
   projectId: '1wi9s7',
@@ -55,15 +7,15 @@ export default defineConfig({
       url: '/api/__coverage__',
     },
   },
+
   e2e: {
+    // specPattern:"cypress/e2e/testFiles.cy.js",
+    experimentalRunAllSpecs:true,
     baseUrl: 'http://localhost:3000/',
     supportFile: 'cypress/support/e2e.ts',
-    experimentalRunAllSpecs: true,
-    specPattern: [
-      'cypress/e2e/*.cy.ts',
-      'cypress/e2e/*.feature', 
-      'cypress/e2e/*.ts'
-    ],
-    setupNodeEvents,
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
   },
 });
