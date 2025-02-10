fbd72cd36064b16b223ca2ef8f3cf50133ccdc48
emanmhmoud <eman.mahmoud@atomica.ai>
2025-02-10 15:49:08 +0200

    added some tests of jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from '@jest/types';
-import "@testing-library/jest-dom/extend-expect";
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/pages/**/*.(spec|test).[jt]s?(x)'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
